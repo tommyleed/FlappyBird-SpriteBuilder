a59c971b09965156e5cd6a8e5e6084087c53a00b
tommyleed <tommyleedonaghy@gmail.com>
2014-11-02 12:20:00 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -32,7 +32,6 @@
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
-    [character flap];
-    
+    [character flap];    
 }
 @end
